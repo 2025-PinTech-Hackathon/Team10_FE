681a581da8cdb19452a3b38d6023ab015ab903c8
sjm <144190764+SOJAEMIN0906@users.noreply.github.com>
2025-05-16 01:30:57 +0900

    feat: enable chat

--- app/src/main/java/com/example/easynewspaper/Utility/Web.java
+++ app/src/main/java/com/example/easynewspaper/Utility/Web.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Timestamp;
 
 public class Web {
     public static void Login(String loginId, String password, Callback callback) {
@@ -209,6 +210,58 @@ public class Web {
 
             } catch (Exception e) {
                 if (callback != null) {
+                    callback.isFailed();
+                }
+            }
+        }).start();
+    }
+
+    public static void PostChat(long userId, String content, Timestamp date, Callback callback) {
+        new Thread(() -> {
+            try {
+                // 1. URL 지정
+                URL url = new URL("/chat/" + userId + "/send");
+
+                // 2. HttpURLConnection 열기
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json; utf-8");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setDoOutput(true); // POST 전송을 위해 true
+
+                // 3. JSON 객체 생성
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("userId", userId);
+                jsonParam.put("content", content);
+                jsonParam.put("date", date);
+
+                // 4. JSON 데이터 전송
+                try (OutputStream os = conn.getOutputStream()) {
+                    byte[] input = jsonParam.toString().getBytes("utf-8");
+                    os.write(input, 0, input.length);
+                }
+
+                // 5. 응답 코드 확인
+                int code = conn.getResponseCode();
+
+                // 6. 응답 내용 읽기
+                try (BufferedReader br = new BufferedReader(
+                        new InputStreamReader(conn.getInputStream(), "utf-8"))) {
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        response.append(line.trim());
+                    }
+
+                    if (callback != null){
+                        callback.isSuccessed(response.toString());
+                    }
+                }
+
+                conn.disconnect();
+
+            } catch (Exception e) {
+                if (callback != null){
                     callback.isFailed();
                 }
             }
