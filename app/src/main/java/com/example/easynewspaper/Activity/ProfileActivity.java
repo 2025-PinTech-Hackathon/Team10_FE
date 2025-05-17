dc74200a1ca649e570e73bd92fd827f12763fe01
Aqoom01 <rlagusgh789@daum.net>
2025-05-17 10:05:24 +0900

    feat: 포인트 보이게 추가

--- app/src/main/java/com/example/easynewspaper/Activity/ProfileActivity.java
+++ app/src/main/java/com/example/easynewspaper/Activity/ProfileActivity.java
@@ -1,16 +1,25 @@
 package com.example.easynewspaper.Activity;
 
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.easynewspaper.DataStruct.Status;
+import com.example.easynewspaper.Interface.Callback;
 import com.example.easynewspaper.R;
+import com.example.easynewspaper.Utility.StatusCheck;
 import com.example.easynewspaper.Utility.Web;
 
+import org.json.JSONObject;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView PointTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +28,20 @@ public class ProfileActivity extends AppCompatActivity {
 
         MainActivity.getInstance().addActivity(this);
 
-        ((TextView)findViewById(R.id.profileNicknameTxt)).setText(Web.GetNickname() + "님\n안녕하세요!");
+        ((TextView) findViewById(R.id.profileNicknameTxt)).setText(Web.GetNickname() + "님\n안녕하세요!");
+        PointTxt = ((TextView) findViewById(R.id.pointTxt));
+
+        Web.getMyPage(new Callback() {
+            @Override
+            public void isSuccessed(String response) {
+                successedMethod(response);
+            }
+
+            @Override
+            public void isFailed() {
+                MainActivity.getInstance().sendToast("마이페이지를 불러오는 데 실패했습니다.");
+            }
+        });
 
         findViewById(R.id.CloseProfileBtn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +66,39 @@ public class ProfileActivity extends AppCompatActivity {
                 MainActivity.getInstance().openIntent(EIntent.Login);
             }
         });
+    }
+
+    void successedMethod(String response) {
+        try {
+            JSONObject resJson = new JSONObject(response);
+            boolean isSuccess = resJson.getBoolean("isSuccess");
+            int code = resJson.getInt("code");
+
+            if (isSuccess) {
+                Status status = StatusCheck.isSuccess(code);
+
+                if (status.succesed) {
+                    JSONObject data = resJson.getJSONObject("data");
+                    int point = data.getInt("point");
+
+                    initPointView(point);
+                } else {
+                    MainActivity.getInstance().sendToast(status.msg);
+                }
+            }
+        } catch (Exception e) {
+            MainActivity.getInstance().sendToast("올바르지 않은 값입니다.");
+        }
+    }
+
+    void initPointView(int point) {
+        new Thread(() -> {
+            // 백그라운드 작업 수행
+
+            PointTxt.post(() -> {
+                PointTxt.setText("Point : " + point);
+            });
+        }).start();
     }
 
     @Override
